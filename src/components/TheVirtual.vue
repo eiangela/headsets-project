5d2f4a23ef335d78c2ef97553ea42fdd3765de84
Angela R <angelaribeiro2@hotmail.com>
2024-09-06 17:01:45 -0300

    WIP

--- src/components/TheVirtual.vue
+++ src/components/TheVirtual.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="background-container">
-    <div class="d-flex align-center justify-center fill-height">
+    <div class="background-image"></div>
+    <div class="content-overlay d-flex">
       <v-container>
         <v-row>
-          <v-col col="4">
-            <v-card class="align-center float-left pa-10">
-              <v-card-text class="text-uppercase the-technology font-weight-bold"
-                >technology</v-card-text
-              >
-              <v-card-title class="the-virtual-title"
-                >The benefits of <br />virtual reality</v-card-title
-              >
-              <v-card-actions class="pa-4">
+          <v-col cols="12" sm="9" md="8" class="d-flex justify-center justify-md-start ">
+            <v-card class="align-center text-center  float-md-left pa-10 the-virtual-card">
+              <v-card-text class="text-uppercase the-technology font-weight-bold">
+                technology
+              </v-card-text>
+              <v-card-title class="the-virtual-title">
+                The benefits of <br />virtual reality
+              </v-card-title>
+              <v-card-actions class="pa-4 d-flex justify-center">
                 <v-btn
                   variant="outlined"
-                  class="text-lowercase the-virtual-btn font-weight-bold"
+                  class="text-lowercase the-virtual-btn font-weight-bold justify-center"
                   width="150"
                   height="50"
-                  >See more</v-btn
                 >
+                  See more
+                </v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
